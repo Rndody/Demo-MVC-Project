8eaa86c47c79ef6034d73584dcd826bf4e2c306a
rndody <rndody@gmail.com>
2024-03-24 08:50:48 +0200

    Employee Repository and  its interface

---
+++ MVC Project Business Logic Layer/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using MVC_Project_Data_Access_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project_Business_Logic_Layer.Interfaces
+{
+    public interface IEmployeeRepository  //public as we need to use it outside BLL 
+    {
+        //define 5 signatures for 5 methods
+        IEnumerable<Employee> GetAll();
+        Employee Get(int id);
+        int Add(Employee entity);
+        int Update(Employee entity);
+        int Delete(Employee entity);
+    }
+}
---
+++ MVC Project Business Logic Layer/Repositories/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project_Business_Logic_Layer.Interfaces;
+using MVC_Project_Data_Access_Layer.Data;
+using MVC_Project_Data_Access_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project_Business_Logic_Layer.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeeRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public int Add(Employee entity)
+        {
+            dbContext.Employees.Add(entity);
+            return dbContext.SaveChanges();
+        }
+
+        public int Delete(Employee entity)
+        {
+            dbContext.Employees.Remove(entity);
+            return dbContext.SaveChanges();
+        }
+
+        public Employee Get(int id)
+       => dbContext.Employees.Find(id);
+        // var Employee = dbContext.Employees.Where(E => E.Id == id).FirstOrDefault();        
+
+        public IEnumerable<Employee> GetAll()
+        => dbContext.Employees.AsNoTracking().ToList();
+
+
+        public int Update(Employee entity)
+        {
+            dbContext.Employees.Update(entity);
+            return dbContext.SaveChanges();
+        }
+    }
+}
