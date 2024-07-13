3399258c33277cf4c654e89bf4ee063f5beb0212
rndody <rndody@gmail.com>
2024-07-14 00:52:10 +0300

    ..

---
+++ MVC Project Presentation Layer/ViewModels/User/SignUpViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project_Presentation_Layer.ViewModels.User
+{
+	public class SignUpViewModel
+	{
+		[Required(ErrorMessage = "name is requierd")]
+		public string Username { get; set; }
+		[Required(ErrorMessage = "email is requierd")]
+		[EmailAddress(ErrorMessage = "invalid email")]
+		public string Email { get; set; }
+
+
+		[Required(ErrorMessage = "First name is requierd")]
+		[Display(Name = "First Name ")]
+		public string FirstName { get; set; }
+		[Required(ErrorMessage = "Last name is requierd")]
+		[Display(Name = "Last Name ")]
+		public string LastName { get; set; }
+
+
+
+
+		[Required(ErrorMessage = "password is Required")]
+		[MinLength(5, ErrorMessage ="at least 5 chars")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		[Required(ErrorMessage = " Confirming password is Required")]
+		[DataType(DataType.Password)]
+		[Compare(nameof(Password) ,ErrorMessage ="doesn't match"  )]
+		public string ConfirmPassword { get; set; }
+		public bool IsAgree { get; set; }
+
+	}
+}
